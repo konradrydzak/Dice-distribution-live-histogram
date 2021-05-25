e5bf794f4f4b6f5ae1f6e9dd58661d0d1df4c980
Konrad <konradrydzak@gmail.com>
2021-05-25 12:29:12 +0200

    keyword arguments added

--- Dice_distribution_live_histogram.py
+++ Dice_distribution_live_histogram.py
@@ -10,22 +10,22 @@ if __name__ == "__main__":
     max_value = 5 * number_of_dice + 1  # Because the minimal value is sum of ones and there can't be a lower value
     results = [0] * max_value
     calculated_results = [0] * max_value
-    t = np.arange(number_of_dice, max_value + number_of_dice)
+    t = np.arange(start=number_of_dice, stop=max_value + number_of_dice)
 
     for i in range(max_trials):
         result = 0
-        result = np.sum(np.random.randint(1, 6 + 1, number_of_dice))
+        result = np.sum(np.random.randint(low=1, high=6 + 1, size=number_of_dice))
         results[result - number_of_dice] += 1
         if (i + 1) % interval == 0:
             os.system('cls')
             print(f"Results after {i + 1} iterations:")
             for j in range(max_value):
-                calculated_results[j] = round(results[j] / (i + 1) * 100, 3)
+                calculated_results[j] = round(number=results[j] / (i + 1) * 100, ndigits=3)
                 print(str(j + number_of_dice) + ': ' + str(calculated_results[j]) + '%')
             plt.clf()
             plt.plot(t, calculated_results, 'bo')
             plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter())
-            plt.grid(True)
+            plt.grid(b=True)
             plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
             plt.minorticks_on()
             plt.ion()
